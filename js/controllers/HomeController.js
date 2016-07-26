56205cc4d637af4890011f9a8b65e6dbf64eb2c0
matthew-h-wang <mail4matthew@gmail.com>
2016-07-25 23:43:30 -0400

    try pokeapi

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,13 +1,13 @@
 app.controller('HomeController',['$scope','$http',
 function($scope,$http) {
-	$http.get('http://api.openweathermap.org/data/2.5/forecast/city?id=4930956&APPID=c831e018cf06d3e9faa8291c86bb7d14')
+	$http.get('https://pokeapi.co/api/v2/pokemon/4/')
         .success(function(data) {
-            $scope.greeting = data;
+            $scope.greeting = data.name;
         });
 
 //        4930956
 //        c831e018cf06d3e9faa8291c86bb7d14
 //http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
-//http://api.openweathermap.org/data/2.5/forecast/city?id=4930956&APPID=c831e018cf06d3e9faa8291c86bb7d14
+//https://api.openweathermap.org/data/2.5/forecast/city?id=4930956&APPID=c831e018cf06d3e9faa8291c86bb7d14
 
 }]);
