2e04e8dda2d87e23353e6beb950d4b4fc5f54477
matthew-h-wang <mail4matthew@gmail.com>
2016-07-25 22:40:56 -0400

    'ARGH'

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ var app = angular.module('myWebsite', ['ngRoute']);
 
 app.config(function($routeProvider){
 		$routeProvider
-			.when('/about', {templateUrl:'partials/about.html'})
-			.when('/experience',{templateUrl:'partials/experience.html'})
-			.otherwise({redirectTo:'/home',templateUrl:'partials/home.html'});
+			.when('/', {
+				templateUrl:'partials/home.html'
+			})
+			.when('/about', {
+				templateUrl:'partials/about.html'
+			})
+			.when('/experience',{
+				templateUrl:'partials/experience.html'
+			})
+			.otherwise({
+				redirectTo:'/'
+			});
 	});
