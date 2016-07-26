b2d394e016b371c70146fcdff8650ac6f42d32a1
matthew-h-wang <mail4matthew@gmail.com>
2016-07-25 23:20:19 -0400

    try weather request

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,7 +1,13 @@
 app.controller('HomeController',['$scope','$http',
 function($scope,$http) {
-	$http.get('https://rest-service.guides.spring.io/greeting').
-        success(function(data) {
+	$http.get('http://api.openweathermap.org/data/2.5/forecast/city?id=4930956&APPID=c831e018cf06d3e9faa8291c86bb7d14')
+        .success(function(data) {
             $scope.greeting = data;
         });
+
+//        4930956
+//        c831e018cf06d3e9faa8291c86bb7d14
+//http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+//http://api.openweathermap.org/data/2.5/forecast/city?id=4930956&APPID=c831e018cf06d3e9faa8291c86bb7d14
+
 }]);
