2a449f45f6fda15b5f9ae0f7b9547570d7942215
matthew-h-wang <mail4matthew@gmail.com>
2016-07-26 22:57:15 -0400

    localStorage test

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,12 +1,21 @@
-app.controller('MainController',['$scope', '$location',
-function($scope, $location) {
+app.controller('MainController',['$scope', '$location', '$localStorage',
+function($scope, $location, $localStorage) {
 	$scope.setRoute = function(route) {
 		$location.path(route);
 	}
 
-	$scope.input = {
-		num: '1',
-		word: 'Hello'
-	}
-	
+
+	$scope.$storage = $localStorage.$default({
+          input : {
+          	num: 1,
+          	word: 'Hello'
+          }
+        });
+
+	$scope.input = $localStorage.input;
+	$scope.input.num += 1
+	$scope.updateStorage = function(){
+		$scope.$storage.input = $scope.input
+	};
+
 }]);
